40c0d15ad25b774fa161371c7d8c367c25874b16
SaifurR <90628681+SaifurRR@users.noreply.github.com>
2023-08-31 02:03:10 -0700

    Update graduate_admission_kaggle.py

--- graduate_admission_kaggle.py
+++ graduate_admission_kaggle.py
@@ -14,29 +14,28 @@ from sklearn.preprocessing import Normalizer
 from sklearn.metrics import r2_score
 
 
-# load admissions data
+#load admissions data
 admissions_data = pd.read_csv("admissions_data.csv")
 #print(admissions_data.head())
 
 #admissions_data.describe()
 #print(admissions_data.shape)
 
-# mark predicted values
+#mark predicted values
 labels = admissions_data.iloc[:,-1]
 #print(labels.describe())
 
-# mark features
+#mark features
 features = admissions_data.iloc[:, 1:8]
 
 #split our training and test set
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.25, random_state = 42)
 
-# standardizing our data by scaling it
+#standardizing our data by scaling it
 sc = StandardScaler()
 features_train_scale = sc.fit_transform(features_train)
 features_test_scale = sc.transform(features_test)
 
-# commented out code for if you want to check out the scaled data
 
 #features_train_scale = pd.DataFrame(features_train_scale, columns = features_train.columns)
 #features_test_scale = pd.DataFrame(features_test_scale, columns = features_test.columns)
@@ -44,7 +43,7 @@ features_test_scale = sc.transform(features_test)
 #print(features_train_scale.describe())
 #print(features_test_scale.describe())
 
-# function to design the model
+#function to design the model
 def design_model(feature_data):
 	model = Sequential()
 	num_features = feature_data.shape[1]
@@ -67,29 +66,29 @@ def design_model(feature_data):
 	return model
 
 
-# apply the model to the scaled training data
+#apply the model to the scaled training data
 model = design_model(features_train_scale)
 #print(model.summary())
 
-# apply early stopping for efficiency
+#apply early stopping for efficiency
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
 
-# fit the model with 100 epochs and a batch size of 8
+#fit the model with 100 epochs and a batch size of 8
 # validation split at 0.25
 history = model.fit(features_train_scale, labels_train.to_numpy(), epochs=100, batch_size=8, verbose=1, validation_split=0.25, callbacks=[es])
 
-# evaluate the model
+#evaluate the model
 val_mse, val_mae = model.evaluate(features_test_scale, labels_test.to_numpy(), verbose = 0)
 
-# view the MAE performance
+#view the MAE performance
 print("MAE: ", val_mae)
 
-# evauate r-squared score
+#evauate r-squared score
 y_pred = model.predict(features_test_scale)
 
 print(r2_score(labels_test,y_pred))
 
-# plot MAE and val_MAE over each epoch
+#plot MAE and val_MAE over each epoch
 fig = plt.figure()
 ax1 = fig.add_subplot(2, 1, 1)
 ax1.plot(history.history['mae'])
@@ -99,7 +98,7 @@ ax1.set_ylabel('MAE')
 ax1.set_xlabel('epoch')
 ax1.legend(['train', 'validation'], loc='upper left')
 
-# Plot loss and val_loss over each epoch
+#plot loss and val_loss over each epoch
 ax2 = fig.add_subplot(2, 1, 2)
 ax2.plot(history.history['loss'])
 ax2.plot(history.history['val_loss'])
